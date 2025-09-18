7dd67ce5ce0072ba194f60c119b87e0c0249d45c
kavinkishore6 <72402904+kavinkishore6@users.noreply.github.com>
2025-09-18 13:10:53 +0530

    FeedBackServlet.java

--- FeedBackServlet.java
+++ FeedBackServlet.java
@@ -7,7 +7,7 @@ public class FeedbackServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
-        // Get form data
+        
         String name = request.getParameter("name");
         String book = request.getParameter("book");
         String feedback = request.getParameter("feedback");
@@ -16,11 +16,11 @@ public class FeedbackServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         try {
-            // Load MySQL driver
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
 
 
-            // Connect to DB (update username & password if needed)
+           
 Connection con = DriverManager.getConnection(
     "jdbc:mysql://localhost:3306/feedback_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
     "root", "Kavin@6"
@@ -28,7 +28,7 @@ Connection con = DriverManager.getConnection(
 
 
 
-            // Insert query
+          
             String sql = "INSERT INTO feedback (name, book_name, feedback) VALUES (?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, name);
@@ -38,7 +38,7 @@ Connection con = DriverManager.getConnection(
             ps.executeUpdate();
             con.close();
 
-            // Response
+            
             out.println("feedback saved");
 
         } catch (Exception e) {
